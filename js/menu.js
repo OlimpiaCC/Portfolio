49a0dc3e696775413cf87606108b8592a5dea4d7
olimpiaczech <olimpia.czech@gmail.com>
2020-11-08 13:53:12 +0000

    .

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,17 @@
 const iconOpen = document.querySelector(".open");
 const iconClose = document.querySelector(".close");
-
 const menu = document.querySelector(".menu-open");
+const menuPositions = [...document.querySelectorAll('.menu-open__items a')];
+
+
+
+
+
+menuPositions.forEach(menuPosition => menuPosition.addEventListener('click',
+    () => {
+        menu.classList.toggle('menu-open__active')
+    }
+));
 
 iconOpen.addEventListener("click", () => menu.classList.toggle("menu-open__active"));
-
 iconClose.addEventListener("click", () => menu.classList.toggle("menu-open__active"));
-
-
